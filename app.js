f41b09a14762c3e1b506d2d87d7679c9eeecc008
Juan Nobert <nobert407@gmail.com>
2022-09-20 21:02:15 -0300

    Fetch products implemented

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const vue = new Vue({
     el: "#app",
     data : {
-        
+        produtos : []
+    },
+    methods:{
+        async listarProdutos(){
+            let response = await fetch("./api/produtos.json")
+            let json = await response.json()
+            this.produtos = json;
+        }
+    },
+    created(){
+        this.listarProdutos()
     }
 })
