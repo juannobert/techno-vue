457787479925c03226cb2e256f878db7745a8789
Juan Nobert <nobert407@gmail.com>
2022-09-21 09:59:35 -0300

    Close modal and animation on close

--- app.js
+++ app.js
@@ -19,6 +19,23 @@ const vue = new Vue({
             let response = await fetch(`./api/produtos/${id}/dados.json`)
             let json = await response.json()
             this.produto = json;
+        },
+        abrirModal(id){
+            this.buscarPorId(id);
+            window.scrollTo({
+                top : 0,
+                behavior : "smooth"
+            })
+
+
+        },
+        fecharModal({target,currentTarget}){
+            /*
+            console.log("Target: ",target)//Retorna o elemento clicado (Não precisa ser necessariamente a section)
+            console.log("Current Target",currentTarget)//Retorna sempre a section (Elemento onde está o evento)
+            */
+           if(target === currentTarget) this.produto = false
+
         }
     },
     created(){
